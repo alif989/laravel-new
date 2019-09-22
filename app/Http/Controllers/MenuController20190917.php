dd035de5fbc4af202d3e4ae42c15ca3b152a2f8d
alif_apsis <motaleb.aji@apsissolutions.com>
2019-09-22 18:22:02 +0600

    alif

---
+++ app/Http/Controllers/MenuController20190917.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+use DB;
+use App\Menu;
+class MenuController extends Controller
+{
+//    public function manageMenu(){
+//        $categories =  DB::table('menus')
+//            ->get();
+// //      $categories = menu::where('parents_menu_id', '=', 0)->get();
+//      dd($categories);
+//        if(is_array($categories)){
+//            foreach($categories as $category){
+//
+//            }
+//
+//        }
+//         $childmenu =DB::table('menu')
+//             ->where('menu_type' , '=' ,'sub')
+//             ->get();
+//         //dd($childmenu);
+//         return view('menu.menu',compact('categories','childmenu'));
+//    }
+    public function manageMenu(){
+        $arr = array(
+            'AA' => array(
+                'a1' => 'america',
+                'a2' => 'afganisthan'
+            ),
+            'BB' => array(
+                'b1' => array(
+                    'b11' => 'banana',
+                    'b12' => 'bangi'
+                ),
+                'b2' => array(
+                    'b21' => 'begun',
+                    'b22' => 'borboti',
+                    'b23' => array(
+                        'b231' => 'marigold',
+                        'b232' => 'rose',
+                        'b233' => 'tulip'
+                    )
+                ),
+                'b3' => 'belgium'
+            ),
+            'CC' => 'canada',
+            'DD' => 'denmark'
+        );
+
+        $this->doRecursion($arr);
+    }
+
+    /**
+     * @param $arr
+     */
+    public function doRecursion($a, $k = ''){
+        if(is_array($a)){
+            if($k != '') echo '<li>'.$k.'</li>';
+            echo '<ul>';
+            foreach ($a as $key => $coa){
+                $this->doRecursion($coa, $key);
+            }
+            echo '</ul>';
+        }else{
+            echo '<li>'.$a.'</li>';
+        }
+    }
+
+}
